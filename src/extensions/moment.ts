7fbf265579f9ddd1166967413d42fbc3be7cb378
Yizhen Zhao <yz@ark7.com>
2021-01-24 20:48:53 -0800

    Optional moment value.

--- src/extensions/moment.ts
+++ src/extensions/moment.ts
@@ -6,15 +6,23 @@ import { A7Model, CombinedModelField, ModelizeOptions } from '../core';
 
 A7Model.provide<Moment>({
   modelize: (x: any, options: ModelizeOptions) =>
-    options.field?.field?.tz === 'UTC' ? moment.utc(x) : moment(x),
+    x == null
+      ? x
+      : options.field?.field?.tz === 'UTC'
+      ? moment.utc(x)
+      : moment(x),
   toObject: (x: Moment, field: CombinedModelField) => {
-    return field.field?.tz === 'UTC' ? x.toISOString() : x.toDate().toString();
+    return x == null
+      ? x
+      : field.field?.tz === 'UTC'
+      ? x.toISOString()
+      : x.toDate().toString();
   },
 });
 
 A7Model.provide<Duration>({
-  modelize: (x: any) => moment.duration(x),
+  modelize: (x: any) => (x == null ? x : moment.duration(x)),
   toObject: (x: Duration) => {
-    return _.isString(x) ? x : x.toISOString && x.toISOString();
+    return x == null ? x : _.isString(x) ? x : x.toISOString && x.toISOString();
   },
 });
