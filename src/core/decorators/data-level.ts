acb8b73d6a585451e2caff1b802312b3d39c2796
Yizhen Zhao <yz@ark7.com>
2021-01-26 10:03:18 -0800

    Add populateLevel configuration.

--- src/core/decorators/data-level.ts
+++ src/core/decorators/data-level.ts
@@ -3,7 +3,38 @@ import * as _ from 'underscore';
 import { Field } from '../fields';
 
 /**
- * Specify the data level of the current field
+ * Config data level for the field.
+ *
+ * Each field will be assigned a level number.  The higher the level number, the
+ * more restrict or confidential the field it is.  We have pre-defined five data
+ * levels:
+ *
+ *   1. BASIC (10) - The basic field that will be used in the most scenarios.
+ *                   Usually, presented when it's referenced by other model.
+ *
+ *   2. SHORT (20) - The fields that are useful for displaying as a list or
+ *                   table. Usually, presented in the find or search endpoints.
+ *
+ *   3. DETAIL (30) - The fields that contains detail information. Usually,
+ *                    presented in the get endpoints.
+ *
+ *   4. CONFIDENTIAL (40) - The fields that contains sensitive information.
+ *                          Usually, not returning to the client or only to
+ *                          admins with special privileges.
+ *
+ *   5. NEVER (50) - The fields that are never returns.
+ *
+ *
+ * **Projection**
+ *
+ * We can do the projection by providing a filter level. Any fields with level
+ * numbers that are smaller or equal to the filter level will be projected. You
+ * can tune the filter level by specifying the passLevelMap in the option.
+ *
+ * **Population**
+ *
+ * For a reference field, when the filter level is greater than the
+ * populateLevel specified by the option, the field will be populated.
  */
 export function Level(level: number | LevelOptions): PropertyDecorator {
   return Field<LevelOptions>(_.isNumber(level) ? { level } : level);
@@ -13,9 +44,18 @@ export interface PassLevelMap {
   [current: number]: number;
 }
 
+/**
+ * Options for the data level.
+ */
 export interface LevelOptions {
+  /** The current level for the field. */
   level?: number;
+
+  /** A map to change the current filter level. */
   passLevelMap?: PassLevelMap;
+
+  /** Minimum level to populate the reference field, default is NEVER. */
+  populateLevel?: number;
 }
 
 export enum DefaultDataLevel {
