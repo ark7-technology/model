a8ff65d0dd5d4fdfa27fdebd497955c43269ea79
Yizhen Zhao <yz@ark7.com>
2021-07-19 07:57:25 -0700

    Add isSameModel function by checking model ids.

--- src/core/decorators/reference.ts
+++ src/core/decorators/reference.ts
@@ -45,3 +45,10 @@ export function idOf<M>(x: Ref<M>, options: IdOfOptions = {}): ID {
 export interface IdOfOptions {
   idField?: string;
 }
+
+export function isSameModel<M>(m1: Ref<M>, m2: Ref<M>): boolean {
+  const id1 = idOf(m1);
+  const id2 = idOf(m2);
+
+  return id1 != null && id2 != null && id1.toString() === id2.toString();
+}
