48d0f0fb4deae82bbb114bd2ce50d11ab761b4ef
ray <r@ark7.com>
2021-10-10 16:20:01 -0700

    Disallow mixin of undefined classes

--- src/core/decorators/mixin.ts
+++ src/core/decorators/mixin.ts
@@ -1,3 +1,4 @@
+import * as _ from 'underscore';
 import { Config } from '../configs';
 import { ModelClass, OptionsResolver } from '../fields';
 import { reverseConcatResolver } from '../resolvers';
@@ -6,6 +7,9 @@ import { reverseConcatResolver } from '../resolvers';
  * Mixin another class.
  */
 export function Mixin(cls: ModelClass<any>): ClassDecorator {
+  if (_.isEmpty(cls)) {
+    throw new Error(`Mixin empty value!`);
+  }
   return Config<MixinOptions>({
     mixinClasses: [cls],
     resolver: reverseConcatResolver('mixinClasses'),
